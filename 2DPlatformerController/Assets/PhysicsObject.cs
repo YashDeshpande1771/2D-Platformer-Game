eae873aeecdb47105e6dcf1fa3a58ebf8ea8f085
YashDeshpande1771 <yashd299792.458@gmail.com>
2020-01-27 19:44:31 -0800

    Tutorial 1 : added gravity to the democrate

---
+++ 2DPlatformerController/Assets/PhysicsObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    protected Vector2 velocity;
+    protected Rigidbody2D rb2d;
+    public float gravityModifier = 1f;    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 move = Vector2.up * deltaPosition.y;
+        movement(move);
+
+    }
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void movement(Vector2 move)
+    {
+        rb2d.position = rb2d.position + move;
+    }
+}
