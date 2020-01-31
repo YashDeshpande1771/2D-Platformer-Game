f2f22126101ac387275dd91424d91f2e3aaa6cd8
YashDeshpande1771 <yashd299792.458@gmail.com>
2020-01-31 02:19:57 -0800

    new feature - added gems

---
+++ 2DPlatformerController/Assets/PlayerScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int collectableCount;
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Collectable")
+        {
+            target.gameObject.SetActive(false);
+            collectableCount++;
+            ScoreScript.scoreValue += 50;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+}
+
---
+++ 2DPlatformerController/Assets/TimeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScript : MonoBehaviour
+{
+    public float timeStart;
+    public Text textBox;
+    public static int numberOfCollectables;
+   
+   
+    void Start()
+    {
+        textBox = GetComponent<Text>();
+        timeStart = Time.time;       
+        numberOfCollectables = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        textBox.text = "Time: " + Mathf.Round( Time.time - timeStart).ToString();
+        if(PlayerScore.collectableCount == numberOfCollectables)
+        {
+            textBox.text = "Finished in " + Mathf.Round(Time.time - timeStart).ToString() + " seconds";
+            Time.timeScale = 0;
+        }
+    }
+}
